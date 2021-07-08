7bf9245ae90fd46ad431a25dffed969227b2f1d0
mahmoudfathy557 <mahmoudfathy557@gmail.com>
2021-07-08 05:31:36 +0200

    push

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+// const url = 'http://localhost:5000/posts';  if u run it locally
+
+// run it online
+const url = 'https://memories-mern-stack-2021.herokuapp.com/'
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
 
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+
+
+
+
 
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
 
